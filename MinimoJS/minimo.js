8cdad62757248d8bd346006e965b9cf5bfd43b14
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-02-25 20:13:33 -0300

    commit 14

--- MinimoJS/minimo.js
+++ MinimoJS/minimo.js
@@ -1,3 +1,7 @@
 
 //Um objeto é uma coleção de propriedades e essa propriedade contem um nome e um valor, ou seja, é uma variavel que contem outras variaveis dentro dela
 //Exemplo: Objeto - Xícara / Propriedades - cor, tamanho, utilidade e etc
+
+let xicara = {
+    
+}
