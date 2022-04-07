bbf5034e685563047544c030caa39f2e3fd8a937
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-04-06 22:15:45 -0300

    commit 26

---
+++ Curso de JavaScript/index.js
@@ -0,0 +1,16 @@
+console.log('Bem vindo ao curso')
+
+//Variaveis
+let idade = 5; 
+console.log(idade);
+
+//Não se pode começar o nome de uma variável com um número e nem espaço
+//Para nomes grandes, colocar assim - ex: nomeCompleto
+
+let nome = 'Roberto'
+console.log(nome);
+
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
+//O let permite que eu redefina os valores
