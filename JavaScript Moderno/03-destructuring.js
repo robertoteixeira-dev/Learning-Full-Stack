9f6d0255eabd0b3b0e007cdf970b721b79c90fc5
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-09-09 16:34:10 -0300

    destructuring

---
+++ JavaScript Moderno/03-destructuring.js
@@ -0,0 +1,16 @@
+const user = {
+    firstName: 'Roberto',
+    age: 25,
+    skills: ['PHP', 'JS'],
+}
+
+const { firstName, age } = user;
+
+console.log(firstName);
+
+// Em um Array
+
+const [ primary, secondary ] = skills;
+
+
+
