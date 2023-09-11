7631a3b50f1c92de878d8f6d16fe2df9c570dee1
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-09-10 22:53:57 -0300

    helpers

---
+++ JavaScript Moderno/08-helpers.js
@@ -0,0 +1,19 @@
+const tech = 'Node.js';
+const techs = ['PHP', 'JS', 'GO'];
+
+// Includes 
+const includesString = tech.includes('o');
+const inscludesArray =  techs.includes('PHP');
+
+// .startsWith
+const startsWith = tech.startsWith('N');
+
+// .endsWith 
+const endsWith = tech.endsWith('s');
+
+
+
+
+
+
+
