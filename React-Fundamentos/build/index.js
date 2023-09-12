df7579a69c0a050620c2fbe668de7368c1fd01de
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-09-11 23:53:17 -0300

    installing babel

---
+++ React-Fundamentos/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = require("react-dom");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_reactDom.ReactDOM.render( /*#__PURE__*/_react["default"].createElement("h1", {
+  id: "title"
+}, "Ol\xE1 Mundo!"), document.getElementById('root'));
