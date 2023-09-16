2422d3f4306b58f412946ae18c8b9a454d7ed938
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-09-16 12:36:15 -0300

    fragment

--- React-Fundamentos/src/App.js
+++ React-Fundamentos/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function App() {
-    return <h1>Component</h1>;
+    return (
+        <>
+            <h1>Component</h1>;
+        </>
+
+        // <React.Fragment>
+        // Nesse jeito podemos passar atributos no fragment
+        // </React.Frag>
+    );
 }
 
 export default App;
