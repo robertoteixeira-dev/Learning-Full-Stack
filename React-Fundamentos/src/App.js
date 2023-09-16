28c919d6e69ae30dff684be57d5fbcd52bf522e9
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-09-15 23:35:05 -0300

    component

---
+++ React-Fundamentos/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+    return <h1>Component</h1>;
+}
+
+export default App;
--- React-Fundamentos/src/index.js
+++ React-Fundamentos/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { ReactDOM } from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <h1 id="title">Olá Mundo!</h1>,
-    document.getElementById('root'),
+    <App />
+    // <h1 id="title">Olá Mundo!</h1>,
+    // document.getElementById('root'),
 );
