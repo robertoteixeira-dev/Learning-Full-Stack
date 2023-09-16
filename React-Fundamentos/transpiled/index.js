a6f382fcf6ccb956dac7736a869713b1049e0015
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2023-09-15 23:15:03 -0300

    config loaders

--- React-Fundamentos/transpiled/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-var _reactDom = require("react-dom");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_reactDom.ReactDOM.render( /*#__PURE__*/_react["default"].createElement("h1", {
-  id: "title"
-}, "Ol\xE1 Mundo!"), document.getElementById('root'));
